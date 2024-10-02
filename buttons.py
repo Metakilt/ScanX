6e6571407783db637daf5693213aeb9621b30c2e
Metakilt <ryankyleperez4@gmail.com>
2024-10-02 10:26:00 +1000

    Changed button.py
    Changed button.py to check credentials from .aws/credentials

--- buttons.py
+++ buttons.py
@@ -2,38 +2,41 @@ import streamlit as st
 import pandas as pd
 import random
 import boto3
-import aws_access
 from io import BytesIO
 
 
 class Shopping:
     def __init__(self, data):
         self.data = data
-        if 'cart' not in st.session_state:
+        if "cart" not in st.session_state:
             st.session_state.cart = {}
 
     def scan_1(self):
         item_index = random.choice(self.data.index)
-        item_name = self.data.iloc[item_index]['Title']
-        item_price = self.data.iloc[item_index]['Price']
-        #self.cart[self.data.iloc[item]['Title']] = [1, self.data.iloc[item]['Price'], self.data.iloc[item]['Price']*1]
+        item_name = self.data.iloc[item_index]["Title"]
+        item_price = self.data.iloc[item_index]["Price"]
+        # self.cart[self.data.iloc[item]['Title']] = [1, self.data.iloc[item]['Price'], self.data.iloc[item]['Price']*1]
         if item_name in st.session_state.cart:
             st.session_state.cart[item_name][0] += 1
             st.session_state.cart[item_name][2] += item_price
         else:
-            st.session_state.cart[item_name] = [1, item_price, item_price*1]
+            st.session_state.cart[item_name] = [1, item_price, item_price * 1]
 
     def scan_2(self):
         item_index = random.choice(self.data.index)
-        item_name = self.data.iloc[item_index]['Title']
-        item_price = self.data.iloc[item_index]['Price']
+        item_name = self.data.iloc[item_index]["Title"]
+        item_price = self.data.iloc[item_index]["Price"]
         randomiser = random.randint(2, 5)
-        #self.cart[self.data.iloc[item]['Title']] = [1*randomiser, self.data.iloc[item]['Price'], self.data.iloc[item]['Price']*randomiser]
+        # self.cart[self.data.iloc[item]['Title']] = [1*randomiser, self.data.iloc[item]['Price'], self.data.iloc[item]['Price']*randomiser]
         if item_name in st.session_state.cart:
             st.session_state.cart[item_name][0] += randomiser
-            st.session_state.cart[item_name][2] += item_price*randomiser
+            st.session_state.cart[item_name][2] += item_price * randomiser
         else:
-            st.session_state.cart[item_name] = [1*randomiser, item_price, item_price*randomiser]
+            st.session_state.cart[item_name] = [
+                1 * randomiser,
+                item_price,
+                item_price * randomiser,
+            ]
 
     def remove(self):
         try:
@@ -49,7 +52,7 @@ class Shopping:
 
     def total(self):
         return round(sum(price[2] for price in st.session_state.cart.values()), 2)
-    
+
     def format(self):
         """result = ""
         if len(self.cart) != 0:
@@ -62,32 +65,30 @@ class Shopping:
         else:
             result+='Nothing in cart.'
             return result"""
-        
+
         if st.session_state.cart:
             for item, details in st.session_state.cart.items():
-                st.write(f"{item}, Quantity: {details[0]}, Price: {details[1]}, Total: {details[2]}")
+                st.write(
+                    f"{item}, Quantity: {details[0]}, Price: {details[1]}, Total: {details[2]}"
+                )
         else:
             st.write("Cart is empty.")
 
 
-def access_bucket(access_key, access_id, bucket_name, bucket_obj, region):
-    session = boto3.Session(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=access_id,
-    region_name=region
-    )
+def access_bucket(bucket_name, bucket_obj):
+    session = boto3.Session()
 
-    s3 = session.client('s3')
+    s3 = session.client("s3")
 
     response = s3.get_object(Bucket=bucket_name, Key=bucket_obj)
-    response_data = response['Body'].read()
+    response_data = response["Body"].read()
 
     data = BytesIO(response_data)
     return data
 
 
-if __name__ == '__main__':
-    data = access_bucket(aws_access.ACCESS_ID, aws_access.ACCESS_KEY, aws_access.BUCKET_NAME, aws_access.BUCKET_OBJ, aws_access.REGION)
+if __name__ == "__main__":
+    data = access_bucket(bucket_name="itpm-products", bucket_obj="Grocery_Dataset.csv")
     df = pd.read_csv(data)
     basket = Shopping(df)
 
@@ -110,10 +111,10 @@ if __name__ == '__main__':
             width: 100%;
         }
         </style>
-        """, 
-        unsafe_allow_html=True
+        """,
+        unsafe_allow_html=True,
     )
-    
+
     st.title("Customer Actions")
 
     with st.container():
@@ -122,23 +123,25 @@ if __name__ == '__main__':
 
         with col1:
             if st.button("SCAN ITEM"):
-                st.session_state['action'] = "Scanned one item..."
+                st.session_state["action"] = "Scanned one item..."
                 basket.scan_1()
         with col2:
             if st.button("SCAN ITEM++"):
-                st.session_state['action'] = "Scanned an item with multiple quantities..."
+                st.session_state["action"] = (
+                    "Scanned an item with multiple quantities..."
+                )
                 basket.scan_2()
         with col3:
             if st.button("REMOVE ITEM"):
-                st.session_state['action'] = "Removed an item..."
+                st.session_state["action"] = "Removed an item..."
                 basket.remove()
         with col4:
             if st.button("CLEAR CART"):
-                st.session_state['action'] = "Cleared cart..."
+                st.session_state["action"] = "Cleared cart..."
                 basket.clear()
 
     # current action
-    if 'action' in st.session_state:
+    if "action" in st.session_state:
         st.write(f"Status: {st.session_state['action']}")
     else:
         st.write("No action taken yet.")
